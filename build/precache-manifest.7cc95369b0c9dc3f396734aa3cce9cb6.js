08078ffaf501d2bfc5f44e742a74057a5482551c
Diego <tucotony1396@gmail.com>
2020-04-29 21:03:27 -0500

    delte build of gitignore

---
+++ build/precache-manifest.7cc95369b0c9dc3f396734aa3cce9cb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c9bed22dd99840c7c3b92f86b59fd29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d4e227058ded314a490",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f085c6f737fcb2dd35e4",
+    "url": "/static/js/2.89ff68a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.89ff68a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d4e227058ded314a490",
+    "url": "/static/js/main.226c4812.chunk.js"
+  },
+  {
+    "revision": "301b30c3b392f36e7aa2",
+    "url": "/static/js/runtime-main.8991914f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
