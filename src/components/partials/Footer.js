d5d0d478d86a4d5cd34a37a08c81fb50bb286805
Diego <tucotony1396@gmail.com>
2020-05-11 23:33:54 -0500

    footer updated

--- src/components/partials/Footer.js
+++ src/components/partials/Footer.js
@@ -1,10 +1,11 @@
 import React, {Component} from "react";
+import './Footer.css';
 class Footer extends Component{
     render() {
         return(
             <footer className="page-footer font-small blue">
                 <div className="footer-copyright text-center py-3">© 2020 Copyright:
-                    <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+                    <a href="https://mdbootstrap.com/"> diegotony</a>
                 </div>
 
             </footer>
