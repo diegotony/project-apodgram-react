9fc94378949e76f2207482794c0cf678d0c5a81b
Diego <tucotony1396@gmail.com>
2020-05-14 22:37:59 -0500

    fuck

--- src/components/images/Image_List.js
+++ src/components/images/Image_List.js
@@ -14,13 +14,11 @@ class ImageList extends Component{
         fetch( "https://apodgram-django-backend.herokuapp.com/images/", {mode:'cors'})
             .then(res => res.json())
             .then(data => {
-
-                let i ;
                 let image_size_10 = []
                 this.setState({images:data})
                 this.setState({length:data.length})
 
-                for (i = 0; i < 10; i++){
+                for (let i = 0; i < 10; i++){
                     image_size_10.push(data[i])
                 }
                 this.setState({imagesResult:image_size_10})
