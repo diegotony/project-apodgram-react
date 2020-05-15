1eabb0b4a84f7e6ce52375d65724aa9c3584d4c2
Diego <tucotony1396@gmail.com>
2020-05-14 23:37:26 -0500

    more details and new image on new tab

--- src/components/images/Image_Card.js
+++ src/components/images/Image_Card.js
@@ -1,41 +1,59 @@
-import React, { useState } from "react";
-import { Card, Button, Container, Row } from 'react-bootstrap'
+import React, { useState, setShow } from "react";
+import { Card, Button, Container, Row, Modal } from 'react-bootstrap'
 import './Image_Card.scss'
-const ImageCard  = (props) => {
+const ImageCard = (props) => {
 
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     const [image] = useState(props.image)
-        return (
-            <Card className="card" >
-                <Card.Img className="img" variant="top" src={image.url} />
-                <Card.Body className="body">
-                    <Card.Title className="title"> <strong>{image.title}</strong>  </Card.Title>
-                    <Card.Text>
-                        {image.explanation.substring(0, 100)}...
+    return (
+        <Card className="card" >
+            <Card.Img className="img" variant="top" src={image.url} />
+            <Card.Body className="body">
+                <Card.Title className="title"> <strong>{image.title}</strong>  </Card.Title>
+                <Card.Text>
+                    {image.explanation.substring(0, 100)}...
                     </Card.Text>
-                    {/* <Card.Subtitle>
+                {/* <Card.Subtitle>
                             {image.date}
                         </Card.Subtitle> */}
 
-                    <Container>
-                        <Row>
-                            <Button className="btn-block view" href={image.hdurl}>View HD</Button>
+                <Container>
+                    <Row>
 
-                            <Button className="btn-block details" href={image.hdurl}>More Details</Button>
+                        <Button className="btn-block view " variant="info" onClick={handleShow}>
+                            More Details
+                             </Button>
+
+                        <Modal show={show} onHide={handleClose}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>{image.title}</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>{image.explanation}</Modal.Body>
+                            <Modal.Footer>
+                                <Button className="btn-block view" onClick={()=> window.open(`${image.hdurl}`, "_blank")}>View HD</Button>
+                                <Button className="btn-block view"  variant="danger" onClick={handleClose}>
+                                    Close
+                                    </Button>
+
+                            </Modal.Footer>
+                        </Modal>
+
+                    </Row>
 
 
-                        </Row>
-
-
-                    </Container>
+                </Container>
 
 
 
 
-                </Card.Body>
-            </Card>
+            </Card.Body>
+        </Card>
 
 
-        )
-    
+    )
+
 }
 export default ImageCard;
