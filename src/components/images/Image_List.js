74c5c17c4e064c56a64f2ac076a56b7c9dd51630
Diego <tucotony1396@gmail.com>
2020-05-14 22:32:02 -0500

    new pagination more eficient

--- src/components/images/Image_List.js
+++ src/components/images/Image_List.js
@@ -5,33 +5,33 @@ import './Image_List.css'
 class ImageList extends Component{
     state = {
         images :[],
-        imagesResult: [],
-        length: 0,
         min: 10,
+        pag:2
 
     }
     componentDidMount() {
         fetch( "https://apodgram-django-backend.herokuapp.com/images/", {mode:'cors'})
             .then(res => res.json())
             .then(data => {
-
-                let i ;
-                let image_size_10 = []
-                this.setState({images:data})
-                this.setState({length:data.length})
-
-                for (i = 0; i < 10; i++){
-                    image_size_10.push(data[i])
-                }
-                this.setState({imagesResult:image_size_10})
-            
+                this.setState({images:data.results})
             }).catch()
     }
 
     addFive(){
-        this.setState({imagesResult:this.state.images.splice(0,this.state.min + 5)})
-        this.setState({min:this.state.min + 5 })
-        window.scrollTo({behavior:'smooth'})
+        fetch(`http://apodgram-django-backend.herokuapp.com/images/?page=${this.state.pag}`,  {mode:'cors'})
+        .then(res => res.json())
+        .then(data => {
+            for(let i = 0; i < 10 ; i++){
+                this.state.images.push(data.results[i])
+                // console.log("==> ",data.results[i])
+                // this.setState({images:this.state.images.push(data.results[i])})
+            }
+
+            this.setState({pag:this.state.pag+1})
+        })
+        // this.setState({imagesResult:this.state.images.splice(0,this.state.min + 5)})
+        // this.setState({min:this.state.min + 5 })
+        // window.scrollTo({behavior:'smooth'})
     }
 
     
@@ -40,7 +40,7 @@ class ImageList extends Component{
         return(
             <div id="columns">
                 <CardColumns>
-                    {this.state.imagesResult.map((e)=>{
+                    {this.state.images.map((e)=>{
                         return (<ImageCard image={e} key={e.id}/>)
                     })}
                 </CardColumns>
