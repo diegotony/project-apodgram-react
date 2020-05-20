90458b248b7a38ddab1c44bca2d93639d4ab4229
Diego <tucotony1396@gmail.com>
2020-05-20 18:03:21 -0500

    Liked button added

--- src/components/images/Image_Card.js
+++ src/components/images/Image_Card.js
@@ -3,31 +3,34 @@ import React, { useState, setShow } from "react";
 import { Card, Button, Container, Row, Modal } from "react-bootstrap";
 import "./Image_Card.scss";
 const ImageCard = (props) => {
-  // eslint-disable-next-line
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  // eslint-disable-next-line
   const [image, setImage] = useState(props.image);
-  let [like, setLike] = useState(props.image.like)
-  let check = false;
+  // eslint-disable-next-line
+  const [like, setLike] = useState(props.image.like);
+  // eslint-disable-next-line
+  const [liked, setLiked]= useState(false)
 
-  function giveLike (id){
-    //   https://apodgram-django-backend.herokuapp.com/like/87/
-    fetch(`https://apodgram-django-backend.herokuapp.com/like/${id}/`,  {mode:'cors'})
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
+  function giveLike(id) {
+    fetch(`https://apodgram-django-backend.herokuapp.com/like/${id}/`, {
+      mode: "cors",
     })
+      .then((res) => res.json())
+      .then((data) => {});
+      setLiked(true)
 
   }
+
+
 
   return (
     <Card className="card">
       <Card.Img className="img" variant="top" src={image.url} />
       <Card.Body className="body">
         <Card.Title className="title">
-          
           <strong>{image.title}</strong>
         </Card.Title>
         <Card.Text>{image.explanation.substring(0, 100)}...</Card.Text>
@@ -47,23 +50,24 @@ const ImageCard = (props) => {
               </Modal.Header>
               <Modal.Body>{image.explanation}</Modal.Body>
               <Modal.Footer className="buttons">
-                  <Button
-                    className="btn btn-primary view"
-                    onChange={() =>giveLike(`${image.id}`)}
-                  > Like 
-                  {like}
-                 
-                    {/* {check ? `Like Y${image.like} + 1`: `Like N ${image.like}`} */}
-                  </Button>
-                  <Button
-                    className="btn-warning view"
-                    onClick={() => window.open(`${image.hdurl}`, "_blank")}
-                  >
-                    View HD
-                  </Button>
-                  <Button className="btn btn-danger view" onClick={handleClose}>
-                    Close
-                  </Button>
+                <Button
+                  className="btn btn-primary view"
+                  disabled={liked}
+                  onClick={() => {giveLike(`${image.id}`)
+                }}
+                >
+                  {liked ? 'Unlike': 'Like'}
+                  {/* {check ? `Like Y${image.like} + 1`: `Like N ${image.like}`} */}
+                </Button>
+                <Button
+                  className="btn-warning view"
+                  onClick={() => window.open(`${image.hdurl}`, "_blank")}
+                >
+                  View HD
+                </Button>
+                <Button className="btn btn-danger view" onClick={handleClose}>
+                  Close
+                </Button>
               </Modal.Footer>
             </Modal>
           </Row>
