a38d734ae072bd40b962cb030e34d14ae4cce048
Diego <tucotony1396@gmail.com>
2020-05-21 22:13:38 -0500

    build failed fixed

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container, Row, Jumbotron } from "react-bootstrap";
+import {Jumbotron } from "react-bootstrap";
 import "./About.css";
 class About extends Component {
   render() {
